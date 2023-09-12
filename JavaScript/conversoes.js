9ded2d0d11066785bbf9840445e01eea55e82175
LuisSaudino <luisgustavo.saudino@gmail.com>
2023-09-12 12:48:13 -0300

    conversões

---
+++ JavaScript/conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456
+const numeroString = '456'
+
+console.log(numero === numeroString) //são tipos diferentes
+console.log(numero == numeroString) //valores iguais
+
+    //Number()
+    //String()
+
+console.log(Number(numeroString) + String(numero))
+
+
+// conversão explicita
