b81c71824f388e5fd9abca35182dba03db4455c1
LuisSaudino <luisgustavo.saudino@gmail.com>
2023-09-20 12:23:34 -0300

    operador ternario

---
+++ JavaScript/operacoes.js
@@ -0,0 +1,14 @@
+// == comparação implicita (valores)
+
+const numero = 5
+const texto = '5'
+
+console.log(numero == texto)
+
+// === comparação explicita (tipos)
+
+console.log(numero === texto)
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
