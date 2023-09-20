a76b14512b9a8ca710a9dc17dfdaba9c17964001
LuisSaudino <luisgustavo.saudino@gmail.com>
2023-09-20 15:47:44 -0300

    aprendendo funcoes

---
+++ JavaScript/funcoes.js
@@ -0,0 +1,16 @@
+function imprimiTexto(texto) {
+    console.log(texto)
+}
+
+imprimiTexto(359234)
+imprimiTexto('ola mundo!')
+
+// três formas de escrever funções 
+
+function soma(){
+    const resultado = 2 + 2
+    return resultado
+}
+
+console.log(soma())
+imprimiTexto(soma())
