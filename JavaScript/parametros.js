3ae66f11520e2a951b11b9487ccda379213b7cd4
LuisSaudino <luisgustavo.saudino@gmail.com>
2023-09-21 11:39:51 -0300

    parametros

---
+++ JavaScript/parametros.js
@@ -0,0 +1,17 @@
+function soma(num1 = 0, num2 = 0) {
+    return num1 + num2
+}
+
+console.log(soma(20,45))
+
+function nomeIdade(nome, idade){
+    return `meu nome é ${nome} e minha idade é ${idade}`
+}
+
+console.log(nomeIdade('luis',23))
+
+function multiplica(num1 = 1,num2 = 1){
+    return num1 * num2
+}
+
+console.log(multiplica(soma(2,3),soma(4,5 )))
